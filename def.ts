6c845acf069afed28277290c753f223499106537
varunkumar.martha@everest.engineering <admin@Admins-MacBook-Pro.local>
2024-07-03 00:28:48 +0530

    created all the models/tables

--- def.ts
+++ def.ts
@@ -29,5 +29,162 @@ const User = sequelize.define(
    },
 );
 
-User.sync({force : true});
 
+const Post = sequelize.define(
+    'post',
+    {
+        postid : {
+            type : DataTypes.INTEGER,
+            primaryKey : true,
+            autoIncrement : true 
+        },
+        postContent : {
+            type : DataTypes.TEXT,
+            allowNull : true,
+        },
+        userid : {
+            type : DataTypes.INTEGER,
+            references : {
+                model : User,
+                key : 'userid'
+            },
+            onDelete : 'CASCADE'
+        }
+    },
+    {
+        tableName : 'Posts',
+        timestamps : false,
+    }
+);
+
+const Comment = sequelize.define(
+    'Comment', 
+    {
+        commentid : {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        postid : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Post,
+                key: 'postid',
+            },
+            onDelete: 'CASCADE'
+        },
+        userid  : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'userid',
+            },
+            onDelete: 'CASCADE',
+        },
+        commentcontent : {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        }
+    }, 
+    {
+        tableName: 'comments',
+        timestamps: false,
+    }
+);
+
+const Like = sequelize.define(
+    'Like', 
+    {
+        likeid : {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        postid : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Post,
+                key: 'postid',
+            },
+            onDelete: 'CASCADE'
+        },
+        userid : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'userid',
+            },
+            onDelete: 'CASCADE'
+        }
+    }, 
+    {
+        tableName: 'likes',
+        timestamps: false
+    }
+);
+
+const Follower = sequelize.define(
+    'Follower', 
+    {
+        followerid : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'userid',
+            },
+            onDelete: 'CASCADE'
+        },
+        followedid : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'userid',
+            },
+            onDelete: 'CASCADE'
+        }
+    }, 
+    {
+        tableName: 'followers',
+        timestamps: false,
+        primaryKey: ['followerid', 'followedid']
+    }
+);
+
+const PremiumUser = sequelize.define(
+    'PremiumUser', 
+    {
+        userid : {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            references: {
+                model: User, 
+                key: 'userid',
+            },
+            onDelete: 'CASCADE'
+        },
+        subscriptionplan : {
+            type: DataTypes.STRING(50),
+        }
+    }, 
+    {
+        tableName: 'premiumusers',
+        timestamps: false,
+    }
+);
+
+const synchronizeModels = async () => {
+    try {
+        await sequelize.sync({force : true });
+        console.log('Successfully created models');
+    } catch (err) {
+        console.log('An error occured!!', err);
+    }
+}
+
+synchronizeModels();
